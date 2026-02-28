ca9a6947aad8922e4fd2e43be1d62b9c6da7c748
gustafi <t.gustavogomes@gmail.com>
2026-02-28 14:45:52 -0300

    teste.

--- WebApplication1/DTOs/CategoryResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.DTOs
-{
-    public class CategoryResponseDto
-    {
-        public int Id { get; set; }
-        public string Description { get; set; } = string.Empty;
-        public string Purpose { get; set; } = string.Empty;
-    }
-}
